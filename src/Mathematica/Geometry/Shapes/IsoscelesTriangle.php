044646f8b7d64fad1563a0daaa98c0aff62bbae4
Luan Maik Cordeiro <luanmaik1994@gmail.com>
2016-03-02 14:47:03 -0300

    Correção de comentário
    Correção de comentário

--- src/Mathematica/Geometry/Shapes/IsoscelesTriangle.php
+++ src/Mathematica/Geometry/Shapes/IsoscelesTriangle.php
@@ -31,7 +31,6 @@ class IsoscelesTriangle extends AbstractTriangle
      */
     public function getArea()
     {
-        //10 , 5 = 24.20614591379636
         $height = sqrt(($this->getEdgeY()*$this->getEdgeY()) - (($this->getEdgeX()*$this->getEdgeX())/4));
         $area = ($this->getEdgeX() * $height) / 2;
         return $area;
