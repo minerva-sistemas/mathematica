4411a24f557b436d8a20e7fdf4ec8766073b45d8
Luan Maik Cordeiro <luanmaik1994@gmail.com>
2016-03-02 16:04:03 -0300

    Criado teste unitário para a classe geometrica Circle
    Criado teste unitário para a classe geometrica Circle

---
+++ test/MathematicaTest/Geometry/Shapes/CircleTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace MathematicaTest\Geometry\Shapes;
+
+use Mathematica\Geometry\Shapes\Circle;
+use MathematicaTest\Geometry\Shapes\Base\AbstractShapeTest;
+
+/**
+ * Teste unitário para a forma geométrica do circulo.
+ * @author  Luan Maik Cordeiro <luanmaik1994@gmail.com>
+ * @package MathematicaTest\Geometry\Shapes
+ */
+class CircleTest extends AbstractShapeTest
+{
+    /**
+     * Define a forma geométrica a ser testada.
+     */
+    public function setUp()
+    {
+        // Configura o quadrado
+        $circle = new Circle();
+        $circle->setName('Circulo de Testes');
+        $circle->setRadius(5);
+
+        // Define o circulo a ser testado
+        $this->setShape($circle);
+    }
+
+    /**
+     * Verifica se o calculo da area foi feito corretamente.
+     */
+    public function testCircleAreaCalculation()
+    {
+        $this->assertEquals($this->getShape()->getArea(), 78.5);
+    }
+
+    /**
+     * Verifica se o calculo do perimetro foi feito corretamente.
+     */
+    public function testCirclePerimeterCalculation()
+    {
+        $this->assertEquals($this->getShape()->getPerimeter(), 31.4);
+    }
+
+    /**
+     * Testa as comparações de medidas entre dois objetos.
+     */
+    public function testGeometricalComparation()
+    {
+        // Configura o circulo
+        $tinyCircle = new Circle();
+        $tinyCircle->setName('Circulo de Testes');
+        $tinyCircle->setRadius(2);
+
+        $this->assertTrue($this->getShape()->greaterAreaThan($tinyCircle));
+        $this->assertTrue($this->getShape()->greaterPerimeterThan($tinyCircle));
+
+        $this->assertNotTrue($tinyCircle->greaterAreaThan($this->getShape()));
+        $this->assertNotTrue($tinyCircle->greaterPerimeterThan($this->getShape()));
+    }
+}
